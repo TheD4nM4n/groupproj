bb742ffc297be5b9025e82096c28ffca27c0fd46
Josh <jfeirick@my.harrisburgu.edu>
2023-04-06 11:08:27 -0400

    Added a main() for testing. Made self.deck empty.

--- player.py
+++ player.py
@@ -1,17 +1,26 @@
 import deck
+import stdio
 
 #Goal: create a 'player' class with a deck.
 
 class Player:
 
     def __init__(self):
-        self.deck = deck.makeStandardDeck()
+        self.deck = []
     
     def getDeck(self): return self.deck
 
     def setDeck(self,deck): self.deck = deck
 
+    def __str__(self): return f"Player with a deck of {len(self.deck)} cards."
 
-def main(): return None
+
+def main(): 
+
+    person = Player()
+
+    person.setDeck(deck.makeStandardDeck())
+
+    stdio.writeln(person)
 
 if __name__ == "__main__": main()
