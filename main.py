ebe811ebd26980bab2a17fddf2bba5d6a2001534
Dipendra <dipendradhakal07@gmail.com>
2023-04-06 11:50:12 -0400

    added gitignore and background

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ H_RES = 1280
 V_RES = 720
 
 stddraw.setCanvasSize(H_RES, V_RES)
+stddraw.setXscale(-1, 1)
+stddraw.setYscale(-1, 1)
+stddraw.clear(stddraw.DARK_GREEN)
 
 while True:
     stddraw.show()
