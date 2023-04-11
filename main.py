122b82deab7e2f822cb77050098a89a25a14c72e
Daniel Poe <dpoe@my.harrisburgu.edu>
2023-04-11 11:20:44 -0400

    Added vanity decks to player boards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import stdio
 import stddraw
 import constants as c
+from picture import Picture
 
 stddraw.setCanvasSize(c.H_RES, c.V_RES)
 stddraw.setXscale(0, 1280)
@@ -8,8 +9,8 @@ stddraw.setYscale(0, 720)
 stddraw.clear(stddraw.DARK_GREEN)
 
 stddraw.setPenColor(stddraw.WHITE)
-stddraw.filledRectangle(c.P1_DECK_X, c.P1_DECK_Y, c.DECK_W, c.DECK_H)
-stddraw.filledRectangle(c.P2_DECK_X, c.P2_DECK_Y, c.DECK_W, c.DECK_H)
+stddraw.filledRectangle(c.P1_BOARD_X, c.P1_BOARD_Y, c.BOARD_W, c.BOARD_H)
+stddraw.filledRectangle(c.P2_BOARD_X, c.P2_BOARD_Y, c.BOARD_W, c.BOARD_H)
 
 stddraw.setPenColor(stddraw.BLACK)
 stddraw.setFontSize(28)
@@ -19,5 +20,9 @@ stddraw.text(c.P2_TEXT_X, c.P2_TEXT_Y, "Player 2")
 stddraw.setFontSize(50)
 stddraw.text(c.P1_SCORE_X, c.P1_SCORE_Y, "0")
 stddraw.text(c.P2_SCORE_X, c.P2_SCORE_Y, "0")
+
+stddraw.picture(Picture("./cards/Deck1.gif"), c.P1_DECK_X, c.P1_DECK_Y)
+stddraw.picture(Picture("./cards/Deck2.gif"), c.P2_DECK_X, c.P2_DECK_Y)
+
 while True:
     stddraw.show()
