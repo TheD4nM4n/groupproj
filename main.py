269447be3a0db78f4b89421fc590b48ff772044a
Daniel Poe <dpoe@my.harrisburgu.edu>
2023-04-20 10:49:18 -0400

    Added comments to main.py

--- main.py
+++ main.py
@@ -12,18 +12,23 @@ from graphicalcard import GraphicalCard
 p1_score = 0
 p2_score = 0
 
-
+# Initializing all necessary objects
 draw_button = Button(640, 360, 90, 45, "WAR!")
 player_1, player_2 = player.Player(), player.Player()
 
 p1_card: Card = None
 p2_card: Card = None
 
+# Intial setup, only run once
+
 def setup():
+
+    # stddraw tings
     stddraw.setCanvasSize(c.H_RES, c.V_RES)
     stddraw.setXscale(0, 1280)
     stddraw.setYscale(0, 720)
 
+    # creating decks for both players
     cards = deck.makeStandardDeck()
     random.shuffle(cards)
     halfIndex = int(len(cards) / 2 )
@@ -33,6 +38,8 @@ def setup():
 
 
 def draw_board():
+
+    # Board drawing code, nothing special (thanks keito for the UI idea, and Dipendra for the code :)
     stddraw.setPenColor(stddraw.WHITE)
     stddraw.filledRectangle(c.P1_BOARD_X, c.P1_BOARD_Y, c.BOARD_W, c.BOARD_H)
     stddraw.filledRectangle(c.P2_BOARD_X, c.P2_BOARD_Y, c.BOARD_W, c.BOARD_H)
@@ -46,28 +53,37 @@ def draw_board():
     stddraw.picture(Picture("./cards/Deck2.gif"), c.P2_DECK_X, c.P2_DECK_Y)
 
 def draw_score():
+
+    # Score code, draws in each player's board
     stddraw.setFontSize(50)
     stddraw.text(c.P1_SCORE_X, c.P1_SCORE_Y, str(p1_score))
     stddraw.text(c.P2_SCORE_X, c.P2_SCORE_Y, str(p2_score))
 
 def draw_ui():
+
+    # The only functional UI element is the "WAR!" button, so here you go
     stddraw.setFontSize(20)
     draw_button.draw()
 
 if __name__ == "__main__":
     setup()
     while True:
+
+        # Redraw the whole game every time, because pygame
         stddraw.clear(stddraw.DARK_GREEN)
         draw_board()
         draw_score()
         draw_ui()
 
+        # My favorite part, the logic (thanks Josiah for the logic code!)
         if stddraw.mousePressed():
             m_X = stddraw.mouseX()
             m_Y = stddraw.mouseY()
             if draw_button.clicked(m_X, m_Y):
                 tied = True
                 played_cards = []
+
+                # This continuously draws cards if tied, until one wins
                 while tied:
                     p1_card = player_1.drawCard()
                     p2_card = player_2.drawCard()
@@ -83,6 +99,7 @@ if __name__ == "__main__":
                             played_cards.extend([player_1.drawCard(), player_2.drawCard()]) #Draws one card each and appends it to the list.
                         else: draw_button.deactivate()
         
+        # All I do is win, win, win, no matter what
         if not player_1.getDeck():
             draw_button.deactivate()
             if p1_score > p2_score:
@@ -92,6 +109,7 @@ if __name__ == "__main__":
             else:
                 stddraw.text(640, 72, "It's a draw!")
 
+        # Thanks Prof. Grim for this code, didn't feel like making graphical cards myself
         if isinstance(p1_card, Card):
             GraphicalCard(p1_card.get_rank(), p1_card.get_suit()).draw(640, 180)
             GraphicalCard(p2_card.get_rank(), p2_card.get_suit()).draw(640, 540)
